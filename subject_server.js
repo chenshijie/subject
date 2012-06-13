49ea607219f90ec7ba490a8feb7187108ab9be1c
ChenShijie <shijie.chen@gmail.com>
2012-06-13 13:37:24 +0800

    优化主题分析规则,行曾到A股雷达帐号的发送和转发,新增无单只股票占60%发送到第一支股票其他转发,去除已经退市的股票

--- subject_server.js
+++ subject_server.js
@@ -40,7 +40,6 @@ devent.on('queued', function(queue) {
   }
 });
 
-
 var scws_options = {
   cmd: configs.scws.cmd,
   dict: configs.scws.dict,
@@ -57,157 +56,158 @@ var table_map = configs.table_map;
  * @param callback
  */
 var prepareTask = function(task, callback) {
-    if (configs.debug) {
-      console.log('----------> prepareTask <-----------');
-    }
-    if (task.original_task.retry >= 10) {
+  if (configs.debug) {
+    console.log('----------> prepareTask <-----------');
+  }
+  if (task.original_task.retry >= 10) {
+    var error = {
+      error: 'TASK_RETRY_TIMES_LIMITED',
+      msg: 'try to deal with the task more than 10 times'
+    };
+    callback(error, task);
+  } else {
+    var key = task.hostname + ':' + task.port + ':' + task.database;
+    // if(table_map[task.table] != undefined) {
+    //   task.table = table_map[task.table];
+    // }
+    var db = databases[key];
+    console.log(key);
+    if (db != undefined) {
+      task['mysql'] = db;
+      task['logger'] = _logger;
+      task['debug'] = configs.debug;
+      task['segment'] = segment;
+      task['keyWords'] = keyWords;
+      callback(null, task);
+    } else {
       var error = {
-        error: 'TASK_RETRY_TIMES_LIMITED',
-        msg: 'try to deal with the task more than 10 times'
+        error: 'TASK_DB_NOT_FOUND',
+        msg: 'cant not find the database configs included by task URI'
       };
       callback(error, task);
-    } else {
-      var key = task.hostname + ':' + task.port + ':' + task.database;
-      // if(table_map[task.table] != undefined) {
-      //   task.table = table_map[task.table];
-      // }
-      var db = databases[key];
-      console.log(key);
-      if (db != undefined) {
-        task['mysql'] = db;
-        task['logger'] = _logger;
-        task['debug'] = configs.debug;
-        task['segment'] = segment;
-        task['keyWords'] = keyWords;
-        callback(null, task);
-      } else {
-        var error = {
-          error: 'TASK_DB_NOT_FOUND',
-          msg: 'cant not find the database configs included by task URI'
-        };
-        callback(error, task);
-      }
     }
-  };
+  }
+};
 
 var getCallback = function(info) {
-    return function(err, ret) {
-      if (err == null) {
-        // 所有步骤完成,任务完成
-        console.log(utils.getLocaleISOString() + ' task-finished : ' + info.original_task.uri);
-        devent.emit('task-finished', info.original_task);
-        // 如果页面内容被保存到服务器,将新任务加入到队列
-        if (info.save2DBOK && info.new_task_id > 0) {
-          var new_task = utils.buildTaskURI({
-            protocol: info.protocol,
-            hostname: info.hostname,
-            port: info.port,
-            database: info.database,
-            table: 'article_subject',
-            id: info.new_task_id
-          });
-          console.log(utils.getLocaleISOString() + ' NEW_TASK: ' + new_task);
-          queue4Similar.enqueue(new_task);
-        }
-      } else if (err.error == 'TASK_RETRY_TIMES_LIMITED') {
-        console.log(utils.getLocaleISOString() + ' 任务尝试次数太多,通知队列任务完成,不在继续尝试' + info.original_task.uri);
-        devent.emit('task-finished', info.original_task);
-      } else if (err.error == 'TASK_DB_NOT_FOUND') {
-        console.log(utils.getLocaleISOString() + ' TASK_DB_NOT_FOUND: ' + info.original_task.uri);
-        devent.emit('task-finished', info.original_task);
-      } else if (err.error == 'TASK_MICRO_BLOG_NOT_FOUND') {
-        console.log(utils.getLocaleISOString() + ' TASK_MICRO_BLOG_NOT_FOUND: ' + info.original_task.uri);
-        devent.emit('task-finished', info.original_task);
-      } else if (err.error == 'TASK_ARTICLE_CONTENT_NOT_FOUND') {
-        console.log(utils.getLocaleISOString() + ' TASK_ARTICLE_CONTENT_NOT_FOUND: ' + info.original_task.uri);
-        devent.emit('task-finished', info.original_task);
-      } else if (err.error == 'calculateKeyWordsWeight_ERROR') {
-        console.log(utils.getLocaleISOString() + ' calculateKeyWordsWeight_ERROR: ' + info.original_task.uri);
-      } else if (err.error == 'CONTENT_INCLUDE_MORE_THAN_ONE_BLOCK') {
-        console.log(utils.getLocaleISOString() + ' CONTENT_INCLUDE_MORE_THAN_ONE_BLOCK: ' + info.original_task.uri);
-        devent.emit('task-finished', info.original_task);
-      } else if (err.error == 'CONTENT_BLOCK_NOT_COVER_STOCKS') {
-        console.log(utils.getLocaleISOString() + ' CONTENT_BLOCK_NOT_COVER_STOCKS: ' + info.original_task.uri);
-        devent.emit('task-finished', info.original_task);
-      } else if (err.error == 'CONTENT_INCLUDE_MORE_THEN_TEN_STOCKS') {
-        console.log(utils.getLocaleISOString() + ' CONTENT_INCLUDE_MORE_THEN_TEN_STOCKS: ' + info.original_task.uri);
-        devent.emit('task-finished', info.original_task);
-      } else if (err.error == 'CONTENT_STOCKS_NO_SAME_BLOCK') {
-        console.log(utils.getLocaleISOString() + ' CONTENT_STOCKS_NO_SAME_BLOCK: ' + info.original_task.uri);
-        devent.emit('task-finished', info.original_task);
-      } else if (err.error == 'CONTENT_SINGLE_STOCK_WEIGHT_LESS_60') {
-        console.log(utils.getLocaleISOString() + ' CONTENT_STOCKS_NO_SAME_BLOCK: ' + info.original_task.uri);
-        devent.emit('task-finished', info.original_task);
-      } else {
-        console.log(err);
+  return function(err, ret) {
+    if (err == null) {
+      // 所有步骤完成,任务完成
+      console.log(utils.getLocaleISOString() + ' task-finished : ' + info.original_task.uri);
+      devent.emit('task-finished', info.original_task);
+      // 如果页面内容被保存到服务器,将新任务加入到队列
+      if (info.save2DBOK && info.new_task_id > 0) {
+        var new_task = utils.buildTaskURI({
+          protocol: info.protocol,
+          hostname: info.hostname,
+          port: info.port,
+          database: info.database,
+          table: 'article_subject',
+          id: info.new_task_id
+        });
+        console.log(utils.getLocaleISOString() + ' NEW_TASK: ' + new_task);
+        queue4Similar.enqueue(new_task);
       }
-    };
+    } else if (err.error == 'TASK_RETRY_TIMES_LIMITED') {
+      console.log(utils.getLocaleISOString() + ' 任务尝试次数太多,通知队列任务完成,不在继续尝试' + info.original_task.uri);
+      devent.emit('task-finished', info.original_task);
+    } else if (err.error == 'TASK_DB_NOT_FOUND') {
+      console.log(utils.getLocaleISOString() + ' TASK_DB_NOT_FOUND: ' + info.original_task.uri);
+      devent.emit('task-finished', info.original_task);
+    } else if (err.error == 'TASK_MICRO_BLOG_NOT_FOUND') {
+      console.log(utils.getLocaleISOString() + ' TASK_MICRO_BLOG_NOT_FOUND: ' + info.original_task.uri);
+      devent.emit('task-finished', info.original_task);
+    } else if (err.error == 'TASK_ARTICLE_CONTENT_NOT_FOUND') {
+      console.log(utils.getLocaleISOString() + ' TASK_ARTICLE_CONTENT_NOT_FOUND: ' + info.original_task.uri);
+      devent.emit('task-finished', info.original_task);
+    } else if (err.error == 'calculateKeyWordsWeight_ERROR') {
+      console.log(utils.getLocaleISOString() + ' calculateKeyWordsWeight_ERROR: ' + info.original_task.uri);
+    } else if (err.error == 'CONTENT_INCLUDE_MORE_THAN_ONE_BLOCK') {
+      console.log(utils.getLocaleISOString() + ' CONTENT_INCLUDE_MORE_THAN_ONE_BLOCK: ' + info.original_task.uri);
+      devent.emit('task-finished', info.original_task);
+    } else if (err.error == 'CONTENT_BLOCK_NOT_COVER_STOCKS') {
+      console.log(utils.getLocaleISOString() + ' CONTENT_BLOCK_NOT_COVER_STOCKS: ' + info.original_task.uri);
+      devent.emit('task-finished', info.original_task);
+    } else if (err.error == 'CONTENT_INCLUDE_MORE_THEN_TEN_STOCKS') {
+      console.log(utils.getLocaleISOString() + ' CONTENT_INCLUDE_MORE_THEN_TEN_STOCKS: ' + info.original_task.uri);
+      devent.emit('task-finished', info.original_task);
+    } else if (err.error == 'CONTENT_STOCKS_NO_SAME_BLOCK') {
+      console.log(utils.getLocaleISOString() + ' CONTENT_STOCKS_NO_SAME_BLOCK: ' + info.original_task.uri);
+      devent.emit('task-finished', info.original_task);
+    } else if (err.error == 'CONTENT_SINGLE_STOCK_WEIGHT_LESS_60') {
+      console.log(utils.getLocaleISOString() + ' CONTENT_STOCKS_NO_SAME_BLOCK: ' + info.original_task.uri);
+      devent.emit('task-finished', info.original_task);
+    } else {
+      console.log(err);
+    }
   };
+};
 
 /**
  * 从队列中获取新任务,取到新任务将其压入workFlow队列
  */
 var getNewTask = function() {
-    if (configs.debug) {
-      console.log('----------> getNewTask <-----------');
+  if (configs.debug) {
+    console.log('----------> getNewTask <-----------');
+  }
+  var time_stamp = utils.getTimestamp();
+
+  queue4Subject.dequeue(function(error, task) {
+    if (error != 'empty' && task != undefined) {
+      var time = utils.getTimestamp();
+      var task_obj = utils.parseTaskURI(task, time);
+      workFlow.push(task_obj);
+    } else {
+      // console.log('task queue is empty');
     }
-    var time_stamp = utils.getTimestamp();
+  });
 
-    queue4Subject.dequeue(function(error, task) {
-      if (error != 'empty' && task != undefined) {
-        var time = utils.getTimestamp();
-        var task_obj = utils.parseTaskURI(task, time);
-        workFlow.push(task_obj);
-      } else {
-        // console.log('task queue is empty');
-      }
-    });
-
-  };
+};
 
 var isKeyWordsLoaded = false;
 var keyWords = null;
 var mysql = databases[configs.load_stock_block_db_key];
 var doLoadKeyWords = function(cb) {
-    var async = require('async');
-    async.series({
-      stocks: function(callback) {
-        mysql.getStockNameAndStockCode(function(result) {
-          var stocks = {};
-          var stock_name = '';
-          for (var i = 0, length = result.length; i < length; i++) {
-            stock_name = result[i].stockname.replace(/[\s]/g, '');
-            stocks[stock_name] = result[i].stockcode.toLowerCase();
-          }
-          callback(null, stocks);
-        });
-      },
-      blocks: function(callback) {
-        mysql.getBlockNameAndBlockid(function(result) {
-          var blocks = {};
-          var block_name = '';
-          for (var i = 0, length = result.length; i < length; i++) {
-            block_name = result[i].block_name.replace(/[\s]/g, '');
-            blocks[block_name] = result[i].id;
-          }
-          callback(null, blocks);
-        });
-      },
-      mapping: function(callback) {
-        mysql.getBlockAndStock(function(result) {
-          var mappings = {};
-          var stock_code = '';
-          for (var i = 0, length = result.length; i < length; i++) {
-            stock_code = result[i].stock_code.toLowerCase();
-            mappings[stock_code] = result[i].block_id;
-          }
-          callback(null, mappings);
-        });
-      }
-    }, function(err, results) {
-      cb(results)
-    });
-  }
+  var async = require('async');
+  async.series({
+    stocks: function(callback) {
+      mysql.getStockNameAndStockCode(function(result) {
+        var stocks = {};
+        var stock_name = '';
+        for (var i = 0, length = result.length; i < length; i++) {
+          stock_name = result[i].stockname.replace(/[\s]/g, '');
+          stocks[stock_name] = result[i].stockcode.toLowerCase();
+        }
+        callback(null, stocks);
+      });
+    },
+    blocks: function(callback) {
+      mysql.getBlockNameAndBlockid(function(result) {
+        var blocks = {};
+        var block_name = '';
+        for (var i = 0, length = result.length; i < length; i++) {
+          block_name = result[i].block_name.replace(/[\s]/g, '');
+          blocks[block_name] = result[i].id;
+        }
+        callback(null, blocks);
+      });
+    },
+    mapping: function(callback) {
+      mysql.getBlockAndStock(function(result) {
+        var mappings = {};
+        var stock_code = '';
+        for (var i = 0, length = result.length; i < length; i++) {
+          stock_code = result[i].stock_code.toLowerCase();
+          mappings[stock_code] = result[i].block_id;
+        }
+        callback(null, mappings);
+      });
+    }
+  },
+  function(err, results) {
+    cb(results)
+  });
+}
 
 doLoadKeyWords(function(result) {
   isKeyWordsLoaded = true;
@@ -224,6 +224,8 @@ setInterval(function() {
       getNewTask();
     }
   }
-}, configs.check_interval);
+},
+configs.check_interval);
 
 console.log('Server Started ' + utils.getLocaleISOString());
+
